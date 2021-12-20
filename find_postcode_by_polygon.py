d226c6f0523382c362401d9b4c15242d4ae5a5e7
Ilja Petin <96020308+ipetin@users.noreply.github.com>
2021-12-20 05:41:33 +0000

    Added filter to postcodes range to make it faster

--- find_postcode_by_polygon.py
+++ find_postcode_by_polygon.py
@@ -13,21 +13,23 @@ from ipyleaflet import Map, GeoJSON
 from multiprocessing.managers import BaseManager
 
 def main():
-
+    csvdb="ukpostcodes_full.csv"
     points_list = []
     lonlat_json_list_file = "import.json"
     export_file = "export.txt"
     f = open(lonlat_json_list_file)
     data = json.load(f)
     
-    with open('ukpostcodes.csv', mode='r') as csv_file:
+    with open(csvdb, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         print("Loading files... ")
         lines= len(list(csv_reader))
         print(lines, "lines loaded")
         csv_file.close()
-        
-    with open('ukpostcodes.csv', mode='r') as csv_file:
+    
+    ignored =0 
+    saved = 0 
+    with open(csvdb, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         _last_loaded=0.0
         _loaded=0
@@ -35,15 +37,24 @@ def main():
             _loaded=_loaded+1
             lat = float(row["latitude"])
             lon = float(row["longitude"])
+
+            if round(lat,2)!=51.79:
+                ignored=ignored+1
+                continue
+            if round(lon,2)!=-0.19 and round(lon,2)!=-0.20:
+                ignored=ignored+1
+                continue  
+          # $# import pdb; pdb.set_trace()
             point = Feature(geometry=Point((lat,lon)))
             points_list.append(point)
+            saved=saved+1
             if(_last_loaded+0.003<round(_loaded/lines,3)):
                 _last_loaded = round(_loaded/lines,3)
                 print("caching "+str(round(_loaded/lines,3)*100)+"%")
         csv_file.close()
 
     points = FeatureCollection(points_list)
-
+    print("cached",saved,"ignored",ignored)
 
     poligon_list = []
     print("creating polygon")
@@ -58,9 +69,10 @@ def main():
    
     print("finding points within polygon, this may take a few minutes depending on how big the database is, if its a .csv file it will take a while")
     result = points_within_polygon(points, polyGon)
-    print("FOUND POSTCODES WITHIN POLYGON")
-    print(result)
+    print("SEARCHING FOR POSTCODES WITHIN POLYGON")
+
     found =0 
+    _loaded=0
     for row in result["features"]:
         latitude = row["geometry"]["coordinates"][0]
         longitude = row["geometry"]["coordinates"][1]
@@ -69,7 +81,7 @@ def main():
         lat_dist_down= round(latitude - r_distance,6)
         lon_dist_up= round(longitude + r_distance,6)
         lon_dist_down= round(longitude - r_distance,6)
-        with open('ukpostcodes.csv', mode='r') as csv_file:
+        with open(csvdb, mode='r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             _last_loaded=0.0
             _loaded=0
@@ -85,10 +97,7 @@ def main():
                                 export.write(row["postcode"]+ ' located at ' + str(lat)+":"+str(lon)+"\n") 
                                 export.close() #to change file access mode
                                 print('> ',row["postcode"],' lanlon:',str(lat),'',str(lon))
-                                
-                if(_last_loaded+0.003<round(_loaded/lines,3)):
-                    _last_loaded = round(_loaded/lines,3)
-                    print("postcodes search "+str(round(_loaded/lines,3)*100)+"%", found,"postcodes found")
+
             csv_file.close()   
 
 
